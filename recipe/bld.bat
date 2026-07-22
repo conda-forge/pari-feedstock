fb1b1161ff0dda7cacd482d692e401c7e42139bf
Isuru Fernando <isuruf@gmail.com>
2026-07-22 16:35:25 -0700

    Modify bld.bat to echo target platform
    Updated bld.bat to include target platform echo statements.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,10 @@
-echo source %SYS_PREFIX:\=/%/etc/profile.d/conda.sh    > conda_build.sh
+echo echo "${target_platform}"                         > conda_build.sh
+echo source %SYS_PREFIX:\=/%/etc/profile.d/conda.sh   >> conda_build.sh
+echo echo "${target_platform}"                        >> conda_build.sh
 echo conda activate "${PREFIX}"                       >> conda_build.sh
 echo conda activate --stack "${BUILD_PREFIX}"         >> conda_build.sh
 echo CONDA_PREFIX=${CONDA_PREFIX//\\//}               >> conda_build.sh
+echo echo "${target_platform}"                        >> conda_build.sh
 type "%RECIPE_DIR%\build.sh"                          >> conda_build.sh
 
 set PREFIX=%PREFIX:\=/%
